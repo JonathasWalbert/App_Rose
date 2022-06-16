185140828f3ad2d0ca3fa35798b348d7a51e01c5
Jonathas Walbert <jonathaswalbert1998@gmail.com>
2022-06-16 20:06:44 -0300

    Att 16/06/2022

--- app/src/main/java/com/example/app_rose/FormCadastrar.kt
+++ app/src/main/java/com/example/app_rose/FormCadastrar.kt
@@ -1,44 +1,36 @@
 package com.example.app_rose
 
-import android.app.Activity
 import android.content.Intent
-import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import android.widget.Toast
-import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import android.os.Bundle
+import android.widget.Toast
+import com.example.app_rose.databinding.ActivityFormCadastrarBinding
 import com.example.app_rose.databinding.ActivityFormLoginBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthSettings
 
-class FormLogin : AppCompatActivity() {
+class FormCadastrar : AppCompatActivity() {
     private lateinit var firebaseAuth: FirebaseAuth
-    private lateinit var binding: ActivityFormLoginBinding
+    private lateinit var binding: ActivityFormCadastrarBinding
 
-    val message = arrayOf("Preencha todos os campos","Usuario ou senha incorreto")
+    val message = arrayOf("Preencha todos os campos", "Cadastro realizado com sucesso")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityFormLoginBinding.inflate(layoutInflater)
+        binding = ActivityFormCadastrarBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.hide()
 
-        binding.textTelaCadastro.setOnClickListener {
-            var intent = Intent(this,FormCadastrar::class.java)
-            startActivity(intent)
-        }
 
         firebaseAuth = FirebaseAuth.getInstance()
-        binding.btnEntrar.setOnClickListener {
+        binding.btnCadastrar.setOnClickListener {
+            val name = binding.editName.text.toString()
             val email = binding.editEmail.text.toString()
             val password = binding.editSenha.text.toString()
 
-            if (email.isEmpty() || password.isEmpty()){
+            if (email.isEmpty() || password.isEmpty() || name.isEmpty()){
                 Toast.makeText(this, message[0], Toast.LENGTH_SHORT).show()
             } else {
-                firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener{
+                firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener{
                     if(it.isSuccessful){
                         var intent = Intent(this,FormProfile::class.java)
                         startActivity(intent)
--- app/src/main/java/com/example/app_rose/FormProfile.kt
+++ app/src/main/java/com/example/app_rose/FormProfile.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.TextView
 
 class FormProfile : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_profile)
